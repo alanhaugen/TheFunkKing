0763fecefeb60e8a691aec88f82afcbc597c4766
BigBoyGlenn <142876365+BigBoyGlenn@users.noreply.github.com>
2024-01-27 16:09:26 +0100

    Main menu option

--- Assets/Scripts/Main Menu.cs
+++ Assets/Scripts/Main Menu.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    [SerializeField] Slider volumeSlider;
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
  public void PlayGame()
     {
         SceneManager.LoadScene(1);
